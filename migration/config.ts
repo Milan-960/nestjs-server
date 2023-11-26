6007e0c43754b53d02478d892c50d7d9c4df8225
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-11-26 13:01:52 +0100

    🎉 feat: Initial project setup with NestJS, TypeORM, Docker, and PostgreSQL
    📝 docs: Add README.md with basic project information and usage instructions
    
    🔧 chore: Add .env and .env.example files for environment configuration
    
    🔧 chore: Add .eslintrc.js and .prettierrc for code formatting and linting rules
    
    🔧 chore: Add .gitignore to exclude unnecessary files from version control
    
    🔧 chore: Add Dockerfile and docker-compose.yml for containerization
    
    🔧 chore: Add nest-cli.json and tsconfig.json for TypeScript and NestJS configuration
    
    🔧 chore: Add package.json with project dependencies and scripts
    
    🔧 chore: Add database configuration and connection setup
    
    🔧 chore: Add basic app module, controller, and service
    
    🔧 chore: Add initial e2e test setup
    
    🔧 chore: Add migration configuration for database schema changes
    
    🔧 chore: Add initial database entities and subscribers
    
    🔧 chore: Add initial main.ts file for starting the application
    
    This commit sets up the initial project structure and configuration for a NestJS application with a PostgreSQL database, using TypeORM for database interaction and Docker for containerization. It also includes basic linting and formatting rules, as well as a README with basic project information and usage instructions.

---
+++ migration/config.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
+export const MIGRATIONS_TABLE = 'migration';
+
+const config: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env['DB_HOST'],
+  port: +process.env['DB_PORT'],
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD'],
+  database: process.env['DB_NAME'],
+  logging: true,
+  synchronize: false,
+  migrationsTableName: MIGRATIONS_TABLE,
+  entities: [path.join(__dirname, '..', 'src', '**', '*.entity.js')],
+  migrations: [path.join(__dirname, 'history', '*.js')],
+};
+
+export default new DataSource(config);
