886fc7bc953e3a15bbb8ca574f700fd0186cbaaf
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-12-09 18:32:35 +0100

    📦 chore(package.json): add @nestjs/throttler package 🔒 feat(main.ts): enhance CORS policy for better security and control The @nestjs/throttler package was added to the project dependencies to provide rate limiting capabilities. The CORS policy in main.ts was enhanced to restrict CORS to the frontend app domain, which improves security by preventing unauthorized domains from making requests. The policy also specifies allowed headers and methods, and enables preflight requests, which provides more control over the types of requests that the server will accept.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
+
+  // Restrict CORS to your frontend app domain
+  const configService = app.get(ConfigService);
+  const frontendUrl = configService.get('FRONTEND_URL');
+  app.enableCors({
+    origin: frontendUrl,
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 3600,
+    exposedHeaders: 'Authorization',
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
