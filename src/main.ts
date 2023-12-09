96a2a600c803f8f9766d100a5c257a3b979720f7
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-12-09 18:45:30 +0100

    🔥 remove(main.ts): comment out ConfigService and frontendUrl to disable CORS restriction 🔧 refactor(main.ts): set origin to true in app.enableCors to allow requests from all origins The ConfigService and frontendUrl were commented out as they were restricting CORS to a specific frontend domain. This was causing issues with testing and development. To allow requests from all origins, the origin property in app.enableCors was set to true. This change will make the development and testing process smoother and more flexible.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Restrict CORS to your frontend app domain
-  const configService = app.get(ConfigService);
-  const frontendUrl = configService.get('FRONTEND_URL');
+  // const configService = app.get(ConfigService);
+  // const frontendUrl = configService.get('FRONTEND_URL');
   app.enableCors({
-    origin: frontendUrl,
+    origin: true,
     credentials: true,
     allowedHeaders:
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
