d12c443f78d625e5893e4059f4d104d0b9bce1ab
Milan_sacahni <milan.sachani.ms@gmail.com>
2024-08-12 09:05:00 +0200

    📝 (package.json): add new dependencies for fastify support and swagger documentation ✨ (main.ts): integrate fastify support and swagger documentation for API endpoints The package.json file has been updated to include new dependencies for fastify support and swagger documentation. In the main.ts file, fastify support has been integrated by creating a NestFastifyApplication instance and swagger documentation has been added using the @nestjs/swagger package to provide API documentation for the endpoints. This enhances the application by adding support for fastify as a platform and providing detailed API documentation for better understanding and testing of the API endpoints.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 // import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   // Restrict CORS to your frontend app domain
   // const configService = app.get(ConfigService);
   // const frontendUrl = configService.get('FRONTEND_URL');
@@ -19,6 +27,30 @@ async function bootstrap() {
     maxAge: 3600,
     exposedHeaders: 'Authorization',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .setBasePath('api/v1')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
+
+  // Restrict CORS to your frontend app domain
+  // if (process.env.NODE_ENV !== 'production') {
+  //   app.enableCors();
+  // }
 
   await app.listen(process.env.PORT || 3000);
 }
